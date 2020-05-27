ee82d300320e961bdde379f781b254a8cdf38922
thuantv_dr4 <vinhthuan.tr@gmail.com>
2020-05-27 16:43:37 +0700

    install react-native-shimmer-placeholder ==> success

--- checkLibs/src/Main.js
+++ checkLibs/src/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native'
 import {
     Placeholder,
@@ -6,9 +6,13 @@ import {
     PlaceholderLine,
     Fade,
 } from "rn-placeholder";
+import ShimmerPlaceHolder from 'react-native-shimmer-placeholder'
+
 
 
 const Main =()=>{
+
+    const[isLoading, setIsloading] = useState(false)
     return(
         <View style={{alignItems:'center', justifyContent:'center', paddingHorizontal:20}}>
             <Placeholder
@@ -41,8 +45,13 @@ const Main =()=>{
                 <PlaceholderLine width={30} />
             </Placeholder>
 
-            {/*Todo:-----> skeleton*/}
-
+            {/*Todo:-----> ShimmerPlaceHolder*/}
+            <ShimmerPlaceHolder autoRun={true} />
+            <ShimmerPlaceHolder autoRun={true} visible={isLoading}>
+                <Text>
+                    Wow, awesome here.
+                </Text>
+            </ShimmerPlaceHolder>
 
         </View>
     )
