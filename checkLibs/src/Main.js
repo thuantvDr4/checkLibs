391eba27ab7ae08a1cb6a531bbc96fdd04a34cd1
thuantv_dr4 <vinhthuan.tr@gmail.com>
2020-05-27 15:19:04 +0700

    test rn-content-loader ===> ok

--- checkLibs/src/Main.js
+++ checkLibs/src/Main.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native'
-
+import {
+    Placeholder,
+    PlaceholderMedia,
+    PlaceholderLine,
+    Fade,
+} from "rn-placeholder";
 
 
 const Main =()=>{
     return(
-        <View style={{justifyContent:'center'}}>
-            <Text>HOME</Text>
+        <View style={{alignItems:'center', justifyContent:'center', paddingHorizontal:20}}>
+            <Placeholder
+                Animation={Fade}
+                Left={PlaceholderMedia}
+                Right={PlaceholderMedia}
+            >
+                <PlaceholderLine width={80} />
+                <PlaceholderLine />
+                <PlaceholderLine width={30} />
+            </Placeholder>
+
+            <Placeholder
+                Animation={Fade}
+                Left={PlaceholderMedia}
+                Right={PlaceholderMedia}
+            >
+                <PlaceholderLine width={80} />
+                <PlaceholderLine />
+                <PlaceholderLine width={30} />
+            </Placeholder>
+
+            <Placeholder
+                Animation={Fade}
+                Left={PlaceholderMedia}
+                Right={PlaceholderMedia}
+            >
+                <PlaceholderLine width={80} />
+                <PlaceholderLine />
+                <PlaceholderLine width={30} />
+            </Placeholder>
+
+            {/*Todo:-----> skeleton*/}
+
+
         </View>
     )
 }
