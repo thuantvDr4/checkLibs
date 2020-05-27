fdf3508f210544fac13bfb7ef1a0c2bacb1ad805
thuantv_dr4 <vinhthuan.tr@gmail.com>
2020-05-27 14:45:29 +0700

    initial success

---
+++ checkLibs/src/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native'
+
+
+
+const Main =()=>{
+    return(
+        <View style={{justifyContent:'center'}}>
+            <Text>HOME</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Main;
