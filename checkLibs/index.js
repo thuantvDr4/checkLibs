5c3805bdd68e8407b427ca6d414d14969ed2a6c1
thuantv_dr4 <vinhthuan.tr@gmail.com>
2020-05-28 09:53:59 +0700

    initial sreen test ==> ok

--- checkLibs/index.js
+++ checkLibs/index.js
@@ -4,7 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import Main from './src/Main'
+import Main from './src/Main';
+import CheckI18n from './src/CheckI18n'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => CheckI18n);
---
+++ checkLibs/src/CheckI18n.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+
+
+const CheckI18n =()=>{
+    return(
+        <View style={{alignItems:'center'}}>
+            <Text style={{fontSize:20, fontWeight: 'bold'}}>Change Languages</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default CheckI18n
